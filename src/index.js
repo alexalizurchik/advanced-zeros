d34d04268f4472eacb8704d270aa6c3160fa5977
Alexandrina <lizurchik.a@gmail.com>
2018-10-02 00:11:53 +0300

    Add implementation of getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
+module.exports = function getZerosCount(num, base) {
+  let primeFactorsOfBase = getPrimeFactors(base);
+  let zerosByFactor = [];
+  
+  for (let key in primeFactorsOfBase) {
+    let currentNum = num;
+    let count = 0;
+    
+    while (currentNum >= key) {
+      currentNum = parseInt(currentNum / key);
+      count += currentNum;
+    }
+    
+    zerosByFactor.push(parseInt(count / primeFactorsOfBase[key]));
+  }
+
+  return Math.min.apply(null, zerosByFactor);
+};
+
+function getPrimeFactors(num) {
+  let primeFactors = {};
+  
+  for (let i = 2; i <= num; i++) {
+    let count = 0;
+    
+    while (num % i === 0) {
+      num = num / i;
+      count++;
+      primeFactors[i] = count;
+    } 
+  }
+  
+  return primeFactors;
 }
